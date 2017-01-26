837de666a8c6c4892a7b8eb9a10b074bd017f08a
wxh-pc <1393212597@qq.com>
2017-01-26 17:06:29 +0800

    在线简易计算器

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # calculate
+--在线基础计算器
+
+
+## 目录结构说明
+- doc:存放项目文档
+- cssdemo:样式练习demo
+- htmldemo:html标签demo
+- JavaScriptdemo:js语法练习demo
+- src:完整项目代码（计算器代码）
---
+++ src/js/calculate.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 2017/1/26.
+ */
+$(function () {
+    clear();
+
+});
+//清空
+function clear() {
+    $(".clear").click(function () {
+        $(".inputBox").html("")
+    });
+}
