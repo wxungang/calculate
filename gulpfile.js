c7449f1ce6c19c29ead059255f62f173424277e8
xiaogang <wxungang@163.com>
2017-02-25 11:07:15 +0800

    calculate

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+/**
+ * Created by xiaogang on 2017/2/25.
+ */
+"use strict";
+
+
+var gulp = require('gulp');
+
+gulp.task('default', function() {
+    // 将你的默认的任务代码放在这
+    console.log("gulp");
+});
